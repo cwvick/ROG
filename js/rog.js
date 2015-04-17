fbc45fc88ab5372f8d54b466490c5e2b14df0f19
vchuang <vchuang@digitalriver.com>
2015-04-17 11:48:58 +0800

    update

--- js/rog.js
+++ js/rog.js
@@ -1,4 +1,27 @@
 $(function(){
 	// Count down
-	$('#countdown').timeTo(new Date('Thu May 07 2015 00:00:00 GMT+0800 (Taipei Standard Time)')); 
+	$('#countdown').timeTo(new Date('Thu May 07 2015 00:00:00 GMT+0800 (Taipei Standard Time)'));
+
+	$(document).on('click', '.btn-menu-buy, .btn-menu-pro', function(event) {
+		event.preventDefault();
+		$('body').animate({
+			scrollTop: 5547},
+			200, function() {
+			/* stuff to do after animation is complete */
+		});
+	});
+
+	$(document).on('click', '.btn-buy', function(event) {
+		event.preventDefault();
+		$('.lightbox').show();
+	});
+
+	// resize
+	$(window).resize(function() {
+	  if ( $(window).width() < 1024 ) {
+	  	$('body').addClass('mobile');
+	  } else {
+	  	$('body').removeClass('mobile');
+	  }
+	});
 });
