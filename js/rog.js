7849b80afc6c2fd5aa3f0aeb581e0d747a955115
vchuang <vchuang@digitalriver.com>
2015-04-19 21:35:42 +0800

    where to buy

--- js/rog.js
+++ js/rog.js
@@ -1,134 +1,3 @@
-$(function() {
-  // Count down
-  $('#countdown').timeTo(new Date('Thu May 07 2015 00:00:00 GMT+0800 (Taipei Standard Time)'));
-
-  $(document).on('click', '.btn-menu-buy, .btn-menu-pro', function(event) {
-    event.preventDefault();
-    $('body').animate({
-        scrollTop: 5547
-      },
-      200,
-      function() {
-        /* stuff to do after animation is complete */
-      });
-  });
-
-  $(document).on('click', '.btn-buy', function(event) {
-    event.preventDefault();
-    $('.lightbox.toBuy').show();
-  });
-
-  $(document).on('click', '.lightbox .close', function(event) {
-    event.preventDefault();
-    $('.lightbox').hide();
-  });
-
-  $(document).on('click', '.movie_list li', function(event) {
-    event.preventDefault();
-    if ( !$(this).hasClass('selected') ) {
-      $('.movie_list li').removeClass('selected');
-      $(this).addClass('selected');
-
-      var video_id = $(this).data('video-id');
-      loadVideo(video_id);
-    }
-  });
-
-  $(document).on('click', '.scrolldown_container', function(event) {
-    event.preventDefault();
-    $('body').animate({
-      scrollTop: $('.video').offset().top + $('.video').outerHeight()
-    },
-    500,
-    function() {
-      $('.scrolldown_container').css('bottom', $('.index').outerHeight() - $('.video').offset().top -$('.video').outerHeight());
-    });
-  });
-
-  var loadVideo = function(video_id) {
-    if ( !YT_player || !YT_player.getIframe() ) {
-      YT_player = new YT.Player('video_player', {
-        height: $('.screen .video').outerHeight(),
-        width: $('.screen .video').outerWidth(),
-        videoId: video_id,
-        playerVars: {
-          // 'showinfo': 0,
-          // 'controls': 0,
-          // 'modestbranding': 1,
-          // 'rel': 0
-        },
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
-      });
-    } else {
-      YT_player.loadVideoById(video_id);
-    }
-  };
-
-  // resize
-  $(window).resize(function() {
-    if ($(window).width() < 1024) {
-      $('body').addClass('mobile');
-    } else {
-      $('body').removeClass('mobile');
-    }
-
-    // resize youtube player
-    if ( YT_player ) {
-      YT_player.setSize($('.screen .video').outerWidth() , $('.screen .video').outerHeight());
-    }
-
-    bageHandler();
-  });
-
-  var setBuyLinkData = function() {
-  	$.ajax({
-      url: 'buyLink.json'
-    })
-    .done(function(data) {
-      console.log(data);
-    });
-	};
-
-  $(window).scroll(function() {
-    bageHandler();
-  });
-
-  var bageHandler = function() {
-    var sectionA_height = $('.container .index').outerHeight();
-    var winHeight = $(window).height();
-    var scrollTop = $(window).scrollTop();
-
-    if ( sectionA_height <= winHeight + scrollTop ) {
-      $('.box-bage, .scrolldown_container').addClass('stop');
-      $('.scrolldown_container').css('bottom', $('.index').outerHeight() - $('.video').offset().top -$('.video').outerHeight());
-    } else {
-      $('.box-bage, .scrolldown_container').removeClass('stop');
-      $('.scrolldown_container').removeAttr('style');
-    }
-  };
-
-  bageHandler();
-
-  var scrolldownHandler = function() {
-    var bgPosition = parseInt($('.btn_scrolldown').css('background-position-y'));
-    var updateH = -91;
-
-    if ( bgPosition <= updateH*3 ) {
-      bgPosition = 0;
-    } else {
-      bgPosition = bgPosition + updateH;
-    }
-
-    $('.btn_scrolldown').css('background-position-y', bgPosition + 'px');
-  };
-
-  var scrolldownTimer = setInterval(function(){ scrolldownHandler(); }, 175);
-
-});
-
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
@@ -178,3 +47,194 @@ function onPlayerStateChange(event) {
 function stopVideo() {
   YT_player.stopVideo();
 }
+
+$(function() {
+  // Count down
+  $('#countdown').timeTo(new Date('Thu May 07 2015 00:00:00 GMT+0800 (Taipei Standard Time)'));
+
+  $(document).on('click', '.btn-menu-buy, .btn-menu-pro', function(event) {
+    event.preventDefault();
+    $('body').animate({
+        scrollTop: $('.product').offset().top
+      },
+      200,
+      function() {
+        /* stuff to do after animation is complete */
+      });
+  });
+
+  $(document).on('click', '.btn-buy', function(event) {
+    event.preventDefault();
+    var countryList = $(this).parents('li.list_item').data('country-list');
+    if (countryList) {
+      setBuyContent(countryList);
+      $('.lightbox.toBuy').show().css('visibility', 'hidden');
+      $('.country_list li').css('height', $('.country_list').height());
+      // var totalWidth = 0;
+      // $('.country_list li').css('height', $('.country_list').height()).each(function(index, el) {
+      //   totalWidth = totalWidth + $(this).outerWidth();
+      // });
+      // // console.log(totalHeight);
+      // $('.country_list').width(totalWidth);
+      $('.lightbox.toBuy').css('visibility', 'visible');
+    }
+  });
+
+  var setBuyContent = function(countryList) {
+    var content = '';
+    $.each(countryList, function(index, country) {
+      content += '<li>' +
+                    '<div class="country_icon ' + country.countryName + '"></div>' +
+                    '<div class="stroe">' +
+                      getStoreContent(country.storeList) +
+                    '</div>' +
+                  '</li>';
+    });
+
+    $('.lightbox.toBuy ul.country_list').html(content);
+  };
+
+  var getStoreContent = function(storeList) {
+    var storeContent = '';
+    $.each(storeList, function(index, store) {
+      if ( store.buyLink ) {
+        storeContent += '<a href="' + store.buyLink + '" class="name" target="_blank">' + store.storeName + '</a>';
+      } else if ( store.variations ) {
+        storeContent += '<div><div class="name">' + store.storeName + '</div>';
+
+        $.each(store.variations, function(index, prod) {
+           storeContent += '<a href="' + prod.buyLink + '" class="variation_name" target="_blank">' + prod.name + '</a>';
+        });
+
+        storeContent += '</div>';
+      }
+    });
+
+    return storeContent;
+  };
+
+  $(document).on('click', '.lightbox .close', function(event) {
+    event.preventDefault();
+    $('.lightbox').hide();
+  });
+
+  $(document).on('click', '.movie_list li', function(event) {
+    event.preventDefault();
+    if ( !$(this).hasClass('selected') ) {
+      $('.movie_list li').removeClass('selected');
+      $(this).addClass('selected');
+
+      var video_id = $(this).data('video-id');
+      loadVideo(video_id);
+    }
+  });
+
+  $(document).on('click', '.scrolldown_container', function(event) {
+    event.preventDefault();
+    $('body').animate({
+      scrollTop: $('.video').offset().top + $('.video').outerHeight()
+    },
+    500,
+    function() {
+      $('.scrolldown_container').css('bottom', $('.index').outerHeight() - $('.video').offset().top -$('.video').outerHeight());
+    });
+  });
+
+  var loadVideo = function(video_id) {
+    YT_player.destroy();
+
+    if ( !YT_player || !YT_player.getIframe() ) {
+      YT_player = new YT.Player('video_player', {
+        height: $('.screen .video').outerHeight(),
+        width: $('.screen .video').outerWidth(),
+        videoId: video_id,
+        playerVars: {
+          // 'showinfo': 0,
+          // 'controls': 0,
+          // 'modestbranding': 1,
+          // 'rel': 0
+        },
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    } else {
+      YT_player.loadVideoById(video_id);
+    }
+
+    $('.screen .video').css('visibility', 'visible');
+  };
+
+  // resize
+  $(window).resize(function() {
+    if ($(window).width() < 1024) {
+      $('body').addClass('mobile');
+    } else {
+      $('body').removeClass('mobile');
+    }
+
+    // resize youtube player
+    if ( YT_player ) {
+      YT_player.setSize($('.screen .video').outerWidth() , $('.screen .video').outerHeight());
+    }
+
+    bageHandler();
+  });
+
+  var setBuyLinkData = function() {
+  	$.ajax({
+      url: 'buyLink.json'
+    })
+    .done(function(data) {
+      $.each(data, function(index, obj) {
+        var prooduct_id = obj.productId;
+        $('.list_item.' + prooduct_id).data('country-list', obj.countryList);
+      });
+    });
+	};
+
+  setBuyLinkData();
+
+  $(window).scroll(function() {
+    bageHandler();
+  });
+
+  var bageHandler = function() {
+    var sectionA_height = $('.container .index').outerHeight();
+    var winHeight = $(window).height();
+    var scrollTop = $(window).scrollTop();
+
+    if ( sectionA_height <= winHeight + scrollTop ) {
+      $('.box-bage, .scrolldown_container').addClass('stop');
+      $('.scrolldown_container').css('bottom', $('.index').outerHeight() - $('.video').offset().top -$('.video').outerHeight());
+    } else {
+      $('.box-bage, .scrolldown_container').removeClass('stop');
+      $('.scrolldown_container').removeAttr('style');
+    }
+  };
+
+  bageHandler();
+
+  var scrolldownHandler = function() {
+    var bgPosition = parseInt($('.btn_scrolldown').css('background-position-y'));
+    var updateH = -91;
+
+    if ( bgPosition <= updateH*3 ) {
+      bgPosition = 0;
+    } else {
+      bgPosition = bgPosition + updateH;
+    }
+
+    $('.btn_scrolldown').css('background-position-y', bgPosition + 'px');
+  };
+
+  var scrolldownTimer = setInterval(function(){ scrolldownHandler(); }, 175);
+
+  $(window).load(function() {
+    // Load the first video
+    $('.movie_list li:first').trigger('click');
+  });
+
+
+});
